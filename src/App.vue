75bab83a91f3c1752fa41139bef437d16c7f7678
SupitchayaKJ <68525333+SupitchayaKJ@users.noreply.github.com>
2020-10-02 13:53:27 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+    <img alt="Vue logo" src="./assets/opp.png">
+    <!--<HelloWorld msg="Welcome to The Anime" />-->
+    <music />
+</div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+//import Anime from './components/Anime'
+import music from './components/music.vue'
+export default {
+    name: 'App',
+    components: {
+        //HelloWorld,
+        //Anime
+        // eslint-disable-next-line vue/no-unused-components
+        music,
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
